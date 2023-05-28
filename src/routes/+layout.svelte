eed10ab956d540eb3e2b21f2ea7eff2ed4dbda40
Vasiliy <prohozhevva@gmail.com>
2023-05-28 19:20:43 +0300

    много изменений

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Hompage">Prohozhev Vasya</a>
+        <div class="nav=links">
+            <a href="/about" class="link">About me</a>
+            <a href="/blog" class="link">Blog</a>
+            <a href="/projects" class="link">My projects</a>
+            <a href="/contact" class="link">Contacts</a>
+        </div>
+    </div>
+</nav>
+<div class="containter">
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
